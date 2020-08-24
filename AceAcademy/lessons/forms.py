5c39403d758ac593a7801d5ddbf702cd65b39af0
alexiahsu <hsu.enyang@gmail.com>
2020-08-24 12:18:10 +0800

    added cloudinary to forms, models and C template

--- AceAcademy/lessons/forms.py
+++ AceAcademy/lessons/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from .models import Lesson
 from django.db.models import Q
+from cloudinary.forms import CloudinaryJsFileField
+
 
 class LessonForm(forms.ModelForm):
     class Meta:
         model = Lesson
-        fields = ('title', 'code', 'desc')
+        fields = ('title', 'code', 'desc', 'cover')
+        
+    cover = CloudinaryJsFileField()
 
 class SearchForm(forms.Form):
     title = forms.CharField(max_length=100, required=False)
--- AceAcademy/lessons/models.py
+++ AceAcademy/lessons/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Lesson(models.Model):
     title = models.CharField(blank=False, max_length=100)
     code = models.CharField(blank=False, max_length=100)
     desc = models.TextField(blank=False)
+    cover = CloudinaryField()
 
     def __str__(self):
         return self.title 
