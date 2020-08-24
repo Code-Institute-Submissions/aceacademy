8d87c472d0d6e09519d4bfb67d9965895b2fc0a8
alexiahsu <hsu.enyang@gmail.com>
2020-08-24 09:52:40 +0800

    consolidated url routes to lessons

---
+++ AceAcademy/lessons/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+import lessons.views
+
+urlpatterns = [
+    path('', lessons.views.index, 
+        name='view_all_lessons'),
+    path('create', lessons.views.create_lesson,
+        name='create_lesson'),
+    path('update/<lesson_id>', lessons.views.update_lesson,
+        name='update_lesson'),
+    path('delete/<lesson_id>', lessons.views.delete_lesson,
+        name='delete_lesson')
+]
