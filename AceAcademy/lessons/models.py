769073b6c583c89ffd024de1a7a2a805fcd8bc5e
alexiahsu <hsu.enyang@gmail.com>
2020-08-26 14:06:10 +0800

    corrected typos and successfully migrated new fields

--- AceAcademy/lessons/models.py
+++ AceAcademy/lessons/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 from djmoney.models.fields import MoneyField
 from cloudinary.models import CloudinaryField
@@ -8,7 +9,7 @@ from django.core.validators import MinLengthValidator
 
 class Instructor(models.Model):
     instructor_full_name = models.CharField(blank=False, max_length=100, validators=[MinLengthValidator(3)])
-    instructor_preferred_name = models.Charfield(blank=False, max_length=100)
+    instructor_preferred_name = models.CharField(blank=False, max_length=100)
     years_experience = models.IntegerField(blank=False)
     instructor_mobile_number = PhoneNumberField(blank=False)
     qualifications = models.TextField(blank=False)
@@ -19,7 +20,7 @@ class Instructor(models.Model):
 class Reviews(models.Model):
     lesson_reviewed = models.ForeignKey('Lesson', on_delete=models.CASCADE)
     reviewer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    review_content = models.textField(blank=False)
+    review_content = models.TextField(blank=False)
     review_date = models.DateField(blank=False)
     rating = models.IntegerField(blank=False)
 
@@ -30,9 +31,9 @@ class Lesson(models.Model):
     title = models.CharField(blank=False, max_length=255)
     desc = models.CharField(blank=False, max_length=255)
     cost = MoneyField(max_digits=5, decimal_places=2, blank=False, default_currency='SGD')
-    syllabus = models.CharField(blank=False, max_lenght=100)
-    tags = models.CharField(blank=False, max_lenght=100)
-    education_level = models.CharField(blank=False, max_lenght=100)
+    syllabus = models.CharField(blank=False, max_length=100)
+    tags = models.CharField(blank=False, max_length=100)
+    education_level = models.CharField(blank=False, max_length=100)
     cover = CloudinaryField()
     instructor = models.ForeignKey('Instructor', on_delete=models.CASCADE)
 
