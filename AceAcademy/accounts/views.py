f2bdd280af99611d2f8b2cd05cca60a2c1cdc5fe
alexiahsu <hsu.enyang@gmail.com>
2020-08-26 23:14:59 +0800

    add user registration/login/logout route

--- AceAcademy/accounts/views.py
+++ AceAcademy/accounts/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            first_name = form.cleaned_data.get('first_name')
+            messages.success(request, 'Account was created for ' + first_name)
+            return redirect('login')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    messages.info(request, 'See you soon !')
+    return redirect('login')
