be4f37d3be1178eb707e66a20c84e05e88d09657
alexiahsu <hsu.enyang@gmail.com>
2020-08-27 00:10:42 +0800

    added decorators for admin_only, user authentication and user types

--- AceAcademy/accounts/views.py
+++ AceAcademy/accounts/views.py
@@ -6,17 +6,22 @@ from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .decorators import unauthenticated_user, allowed_users, admin_only
+from django.contrib.auth.models import Group
 
 
 # Create your views here.
+@unauthenticated_user
 def registerPage(request):
     form = CreateUserForm()
 
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             first_name = form.cleaned_data.get('first_name')
+            group = Group.objects.get(name='user')
+            user.groups.add(group)
             messages.success(request, 'Account was created for ' + first_name)
             return redirect('login')
 
@@ -25,6 +30,7 @@ def registerPage(request):
     }
     return render(request, 'accounts/register.html', context)
 
+@unauthenticated_user
 def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -48,6 +54,7 @@ def logoutUser(request):
     return redirect('login')
 
 @login_required(login_url='login')
+@allowed_users(allowed_roles=['user'])
 def user_main(request):
     return render(request, 'accounts/user_index.html')
 
